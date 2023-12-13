0c3059d70a37c0743e5efbe57097d3edd89b6582
yush <yush2202@gmail.com>
2023-12-13 17:08:19 +0900

    feat: Messages 구현 프로그램에 사용될 Messages를 구현했습니다.

---
+++ src/main/java/bridge/Constants/ErrorMessages.java
@@ -0,0 +1,19 @@
+package bridge.Constants;
+
+public enum ErrorMessages {
+    ERROR_PREFIX("[ERROR] "),
+    INVALID_BLANK_MESSAGE("공백은 입력할 수 없습니다."),
+    INVALID_EMPTY_MESSAGE("아무 값도 입력하지 않았습니다."),
+    INVALID_MOVE_MESSAGE("U 혹은 D만 입력할 수 있습니다."),
+    INVALID_INPUT_MESSAGE("숫자만 입력할 수 있습니다.");
+
+    private String message;
+
+    ErrorMessages(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return ERROR_PREFIX.message + message;
+    }
+}
---
+++ src/main/java/bridge/Constants/OutputMessages.java
@@ -0,0 +1,9 @@
+package bridge.Constants;
+
+public class OutputMessages {
+    public static final String START_GAME = "다리 건너기 게임을 시작합니다.";
+    public static final String ONE_BRIDGE = "[ %s ]\n" + "[ %s ]";
+    public static final String GAME_RESULT = "게임 성공 여부: %s";
+    public static final String TRY_COUNT = "총 시도한 횟수: %d";
+
+}
